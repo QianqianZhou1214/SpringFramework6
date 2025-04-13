e4911cafc2b9e9a68b7cba97745e4e16d5a34dc0
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-13 11:58:01 +0200

    SetUp Integration Test for controller init

---
+++ spring-6-rest-mvc-48-jpa-services/src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+
+
+}
