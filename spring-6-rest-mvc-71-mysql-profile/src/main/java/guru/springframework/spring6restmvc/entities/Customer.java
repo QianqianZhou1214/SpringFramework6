93e06db811fff75e4188b5888b20629532301629
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-18 12:24:18 +0200

    Hikari pool size

--- spring-6-rest-mvc-71-mysql-profile/src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ spring-6-rest-mvc-71-mysql-profile/src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -2,7 +2,9 @@ package guru.springframework.spring6restmvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,6 +22,7 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String name;
