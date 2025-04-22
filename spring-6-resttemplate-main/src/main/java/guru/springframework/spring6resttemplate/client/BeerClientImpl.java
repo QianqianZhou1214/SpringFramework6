9fd78589100002aca36ce8af12adc56ccb4980db
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-22 17:49:36 +0200

    Complete Query Params

--- spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -16,10 +17,16 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-
     private static final String GET_BEER_PATH = "/api/v1/beer";
+
+
     @Override
-    public Page<BeerDTO> listBeers(String beerName) {
+    public Page<BeerDTO> listBeers() {
+        return this.listBeers(null, null, null, null, null);
+    }
+
+    @Override
+    public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize) {
         // get RestTemplate instance from builder
         RestTemplate restTemplate = restTemplateBuilder.build();
 
@@ -28,6 +35,19 @@ public class BeerClientImpl implements BeerClient {
         if(beerName != null) {
             uriComponentsBuilder.queryParam("beerName", beerName);
         }
+        if(beerStyle != null) {
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+        }
+        if(showInventory != null) {
+            uriComponentsBuilder.queryParam("showInventory", showInventory);
+        }
+        if(pageNumber != null) {
+            uriComponentsBuilder.queryParam("pageNumber", pageNumber);
+        }
+        if(pageSize != null) {
+            uriComponentsBuilder.queryParam("pageSize", pageSize);
+        }
+
 
         ResponseEntity<BeerDTOPageImpl> response =
                 restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
