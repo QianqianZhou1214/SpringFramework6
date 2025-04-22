3361fdd33ac371b598117aab01543f7bb37dbe55
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-22 14:55:23 +0200

    Customize URL Path2

--- sdjpa-spring-data-rest-main/src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
+++ sdjpa-spring-data-rest-main/src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@RepositoryRestResource(path = "beer")
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
