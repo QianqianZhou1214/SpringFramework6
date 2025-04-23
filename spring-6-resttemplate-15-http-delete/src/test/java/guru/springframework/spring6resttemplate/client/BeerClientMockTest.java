3bbe0239b553e269bd74e59be098788e1f87773f
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-23 11:15:40 +0200

    Adding Mockito to bind

--- spring-6-resttemplate-15-http-delete/src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ spring-6-resttemplate-15-http-delete/src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -3,39 +3,63 @@ package guru.springframework.spring6resttemplate.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6resttemplate.config.RestTemplateBuilderConfig;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.data.support.PageableExecutionUtils.getPage;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-@RestClientTest(BeerClientImpl.class)
+
+@RestClientTest//(BeerClientImpl.class)
+@Import(RestTemplateBuilderConfig.class)
 public class BeerClientMockTest {
 
     static final String URL = "http://localhost:8080";
 
-    @Autowired
     BeerClient beerClient;
 
-    @Autowired
     MockRestServiceServer server;
 
     @Autowired
+    RestTemplateBuilder restTemplateBuilderConfigured;
+
+    @Autowired
     ObjectMapper objectMapper;
+
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = restTemplateBuilderConfigured.build();
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+        // binding to the RestTemplate, all requests will be taken over by MockRestServer.
+
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder);
+    }
 
     @Test
     void testListBeers() throws JsonProcessingException {
@@ -52,9 +76,7 @@ public class BeerClientMockTest {
 
     }
 
-    BeerDTOPageImpl getPage() {
-        return new BeerDTOPageImpl(Arrays.asList(getBeerDto()), 1, 25, 1);
-    }
+
 
     BeerDTO getBeerDto() {
         return BeerDTO.builder()
@@ -66,5 +88,7 @@ public class BeerClientMockTest {
                 .upc("12345")
                 .build();
     }
-
+    BeerDTOPageImpl getPage() {
+        return new BeerDTOPageImpl(Arrays.asList(getBeerDto()), 1, 25, 1);
+    }
 }
